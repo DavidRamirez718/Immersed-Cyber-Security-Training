aa30297fd4ed7913cd9b2161f05ac89136008898
David Ramirez <65685350+DavidRamirez718@users.noreply.github.com>
2022-12-07 16:02:13 -0600

    Create accessLogsService.js

---
+++ REACT/accessLogsService.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import {onGlobalSuccess, onGlobalError, API_HOST_PREFIX} from "./serviceHelpers";
+
+let getAccessLogs = (pageIndex, pageSize) => {
+    const config = {
+      method: "GET",
+      url: `${API_HOST_PREFIX}/api/accesslogs/paginate?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+      crossdomain: true,
+      headers: { "Content-Type": "application/json" },
+    };
+
+    return axios(config).then(onGlobalSuccess).catch(onGlobalError);
+  };
+
+  let getAccessLogsByDate = (pageIndex, pageSize, startDate, endDate) => {
+    const config = {
+      method: "GET",
+      url: `${API_HOST_PREFIX}/api/accesslogs/dated?pageIndex=${pageIndex}&pageSize=${pageSize}&startDate=${startDate}&endDate=${endDate}`,
+      crossdomain: true,
+      headers: { "Content-Type": "application/json" },
+    };
+
+    return axios(config).then(onGlobalSuccess).catch(onGlobalError);
+  };
+
+  let searchAccessLogs = (pageIndex, pageSize, query) => {
+    const config = {
+      method: "GET",
+      url: `${API_HOST_PREFIX}/api/accesslogs/search?pageIndex=${pageIndex}&pageSize=${pageSize}&query=${query}`,
+      crossdomain: true,
+      headers: { "Content-Type": "application/json" },
+    };
+
+    return axios(config).then(onGlobalSuccess).catch(onGlobalError);
+  }
+
+  let getAccessLogsByEntityOrAccess = (pageIndex, pageSize, entityTypeId, accessStatusId) => {
+    const config = {
+      method: "GET",
+      url: `${API_HOST_PREFIX}/api/accesslogs/entityaccess?pageIndex=${pageIndex}&pageSize=${pageSize}&entitytypeid=${entityTypeId}&accessstatusid=${accessStatusId}`,
+      crossdomain: true,
+      headers: { "Content-Type": "application/json" },
+    };
+
+    return axios(config).then(onGlobalSuccess).catch(onGlobalError);
+  }
+  
+const accessLogsService = {getAccessLogs, getAccessLogsByDate, searchAccessLogs, getAccessLogsByEntityOrAccess};
+
+export default accessLogsService
